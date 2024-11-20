1b7131b6535248c91a5dcb54a4343d09d0f6df71
3107.Pucc <ndp112005@gmail.com>
2024-11-20 22:15:33 +0700

    Lan 3

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
-
-echo "abc";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Danh muc san pham</h1>
+</body>
+</html>
